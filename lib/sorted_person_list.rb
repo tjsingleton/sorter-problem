cb494cf84b9d40b93eed05cc03f53c9f9a47a265
TJ Singleton <tjsingleton@vantagestreet.com>
2011-11-21 14:43:38 -0500

    extract sorter object

--- lib/sorted_person_list.rb
+++ lib/sorted_person_list.rb
@@ -21,9 +21,36 @@ class SortedPersonList
     list
   end
 
-  add_sort(:last_name_desc) {|a,b| b.last_name <=> a.last_name }
-  add_sort(:gender_last_name) {|a,b| [a.gender, a.last_name] <=> [b.gender, b.last_name] }
-  add_sort(:date_last_name) {|a,b| [a.date_of_birth, a.last_name] <=> [b.date_of_birth, b.last_name] }
+  class Sorter
+    DEFAULT_DIRECTION = :asc
+
+    def initialize(*args)
+      options    = args.last.kind_of?(Hash) ? args.pop : {}
+      @direction = options.fetch(:direction) { DEFAULT_DIRECTION }
+      @fields    = args
+    end
+
+    def to_proc
+      @direction == :asc ?  asc_sort : desc_sort
+    end
+
+    private
+    def asc_sort
+      ->(a,b) { select_fields(a) <=> select_fields(b) }
+    end
+
+    def desc_sort
+      ->(a,b) { select_fields(b) <=> select_fields(a) }
+    end
+
+    def select_fields(obj)
+      @fields.map {|n| obj.send(n) }
+    end
+  end
+
+  add_sort :last_name_desc,   &Sorter.new(:last_name, direction: :desc)
+  add_sort :gender_last_name, &Sorter.new(:gender, :last_name)
+  add_sort :date_last_name,   &Sorter.new(:date_of_birth, :last_name)
 
   def initialize(sorter)
     @sorter = sorter
